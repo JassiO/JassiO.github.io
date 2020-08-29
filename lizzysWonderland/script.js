694ae6f24d6a38d6a46f6821f893b96934a54775
JassiO <JassiO@users.noreply.github.com>
2020-08-29 14:46:37 +0200

    Update script.js

--- lizzysWonderland/script.js
+++ lizzysWonderland/script.js
@@ -1,8 +1,8 @@
 const TWO_PI = Math.PI * 2;
 const HALF_PI = Math.PI * 0.5;
 // canvas settings
-var viewWidth = 800px,
-    viewHeight = 800px,
+var viewWidth = 800,
+    viewHeight = 800,
     viewCenterX = viewWidth * 0.5,
     viewCenterY = viewHeight * 0.5,
     drawingCanvas = document.getElementById("drawing_canvas"),
